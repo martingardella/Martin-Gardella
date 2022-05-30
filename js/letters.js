530b965d3fe9a3ce9681323e4bd6ebb07eb778ca
martin <martingardella31@gmail.com>
2022-05-30 16:49:53 -0300

    .

--- js/letters.js
+++ js/letters.js
@@ -1,4 +1,4 @@
-var textWrapper = document.querySelector('.ml1 .letters');
+let textWrapper = document.querySelector('.ml1 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
